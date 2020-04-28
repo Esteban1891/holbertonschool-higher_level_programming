1fe1a39d4a88d16de534554639f18fad884d1352
Esteban1891 <esteban.delahoz15@gmail.com>
2020-04-28 18:06:04 -0500

    task2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 digit = abs(number) % 10
 if number <= 0:
-    digit = digit * 1
-    if digit == 5:
+    digit = digit * -1
+    if digit > 5:
         val = "greater than 5"
     elif digit == 0:
         val = "0"
