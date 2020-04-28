c722ac7367e09851e77fd7897b7d3717a323f97f
Esteban1891 <esteban.delahoz15@gmail.com>
2020-04-28 18:13:09 -0500

    task2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ di = abs(number) % 10
 if number <= 0:
     di = di * -1
 
-if ld > 5:
+if di > 5:
     val = "greater than 5"
-elif ld == 0:
+elif di == 0:
     val = "0"
 else:
     val = "less than 6 and not 0"
