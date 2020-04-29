0c931af07571644ffd9759b54898a6959a5397c0
Esteban1891 <esteban.delahoz15@gmail.com>
2020-04-28 19:54:30 -0500

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter == 101 or letter == 113:
+    if letter == 'q' or letter == 'e':
         continue
 print("{:c}".format(letter), end='')
