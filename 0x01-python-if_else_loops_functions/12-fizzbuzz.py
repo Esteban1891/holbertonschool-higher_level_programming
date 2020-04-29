5c2938f9bee3a363e463b7da8218de992f29bf73
Esteban <60367971+Esteban1891@users.noreply.github.com>
2020-04-28 23:20:39 -0500

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+
+
 def fizzbuzz():
     for num in range(1, 100 + 1):
-    if num % 3 == 0 and num % 5 == 0:
-        n = "FizzBuzz"
+        if num % 3 == 0 and num % 5 == 0:
+            n = "FizzBuzz"
         if num % 3 == 0:
             n = "Fizz"
             if num % 5 == 0:
                 n = "Buzz"
             else:
                 n = num(str)
+
                 print("{:s}".format(n), end='')
