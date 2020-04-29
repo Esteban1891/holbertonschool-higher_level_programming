5127ca6f9757135a0a71a1c773c52c825392e2d0
Esteban <60367971+Esteban1891@users.noreply.github.com>
2020-04-28 20:03:31 -0500

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
   
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter == 101 or letter == 113:
+    if letter == 'q' or letter == 'e':
         continue
-
-    print("{:c}".format(letter), end='')
+print("{:c}".format(letter), end='')
