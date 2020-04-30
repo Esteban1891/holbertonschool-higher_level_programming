db2ec964a50f1ef36735b0fe3d62cf357d857dcf
Esteban1891 <esteban.delahoz15@gmail.com>
2020-04-30 18:17:44 -0500

    ask3

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from hidden_4 import *
-for a in dir():
-if a[:2] != '__':
-    print(a)
+    import hidden_4
+    for a in dir(hidden_4):
+        if a[:2] != '__':
+            print(a)
