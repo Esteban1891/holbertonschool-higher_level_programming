f606e7d7d8fd3061766207538603e384b0af5f89
Esteban1891 <esteban.delahoz15@gmail.com>
2020-04-30 17:27:49 -0500

    ask2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) == 2:
+        print("0 arguments:")
         print("{} {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
