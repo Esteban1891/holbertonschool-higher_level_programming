20a52257fc283a10611dc1c20491d48e3c2f9e93
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-05 20:22:26 -0500

    function that replaces an element in a list at a specific position without modifying the original list (like in C).

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for a in range(len(matrix)):
+        for i in range(len(matrix[a])):
+            print("{}".format(matrix[a][i], end='')
+                    if i > (len(matrix[a]) -1):
+                    print(' ', end='')
+                    print()
+
+
