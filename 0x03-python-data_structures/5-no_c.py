a4533e29742ba01e3b6a1d111a62fae430072b30
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-05 19:39:31 -0500

    function that replaces an element in a list at a specific position without modifying the original list (like in C).

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,7 @@ def no_c(my_string):
     if my_string:
         string = list(my_string)
         for a in range(string):
-            if a in "C":
-            string.remove(a)
-            if a in "c"
+            if a in "C" and a in "c":
             string.remove(a)
             my_string = "".join(string)
             return my_string
