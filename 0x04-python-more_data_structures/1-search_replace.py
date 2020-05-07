837a66481eace35468617411dae872d8045510cf
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-07 17:26:12 -0500

    ask1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return (list(map(lambda new_my_list: new_my_list if new_my_list != search else replace, my_list)))
+    return (list(map(lambda new: new if new != search else replace, my_list)))
