1c89643a52929be254bb15bb8ad40ed2846b8560
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-07 18:33:14 -0500

    ask8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        a_dictionary.remove[key]
+        pow a_dictionary[key]
         return (a_dictionary)
 
