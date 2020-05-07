fb17ccdc07466e6ea99b416b5e18307e6d05f9f9
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-07 16:31:31 -0500

    add ask0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda new_matrix: [a ** 2 for a in new_matrix], matrix))]
+    if matrix:
+    return (list(map(lambda new_matrix: [a ** 2 for a in new_matrix], matrix)))
