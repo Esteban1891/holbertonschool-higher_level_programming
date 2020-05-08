d08536961cd5ef94d061fa992e4681e6189cd5f9
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-07 19:04:54 -0500

    ask10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if type(a_dictionary) is dict and len(a_dictionary):
+    if type(a_dictionary) is dict and len(a_dictionary) > 0:
         return max(a_dictionary)
     else:
         return None
