0ce3607c6f18471a24bc6bd4655b15dc964d7918
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-07 21:27:56 -0500

    advanve

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,4 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    return sum(i[0] * i[1] for i in my_list) / sum(i[1] for i in my_list)]
+    return sum(i[0] * i[1] for i in my_list) / sum(i[1] for i in my_list)
