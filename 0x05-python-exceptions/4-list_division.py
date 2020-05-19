35bbd7d1ce0354ba0184d6d65fe1917c90c708bc
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-18 21:09:07 -0500

    add task4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             count = 0
-        except TypeError:
+        except IndexError:
             print("out of range")
             count = 0
         finally:
