568b077a5afa0dabb971d28f90eb6cb9d60a4a6a
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-18 19:57:40 -0500

    add task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             counter = counter + 1
         except IndexError:
             break
-        print()
-        return counter
+    print()
+    return counter
