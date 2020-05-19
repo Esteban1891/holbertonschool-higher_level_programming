253de34ec882bfbdf5086e3b787d1f718246e00d
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-18 22:46:05 -0500

    add task100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as error:
+        import sys
         print("Exception: {}".format(error), file=sys.stderr)
         return False
     else:
