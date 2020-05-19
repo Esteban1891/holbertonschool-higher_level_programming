4b0f470406ad6afb861a433d28840d6b146e417d
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-18 19:54:52 -0500

    add task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    for a in range(x):
+    for a in range(0, x):
         try:
             print(my_list[a], end="")
             counter = counter + 1
