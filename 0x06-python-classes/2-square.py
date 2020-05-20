34fa49f64ea9bebdd4c437b5110c919e6f974837
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-19 19:37:37 -0500

    add task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,16 @@
+
 #!/usr/bin/python3
-class Square():
-    """A square class."""
+"""Creating a new class called Square"""
+
+
+class Square:
+    """class Square that defines a square by: (based on 1-square.py)"""
     def __init__(self, size=0):
-        """Initialization of the class square."""
-        if type(size) != int:
+        """Initialize Square with size attribute"""
+        self.__size = size
+
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
