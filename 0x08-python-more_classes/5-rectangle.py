e9ef0fee35309b13959f3ff29ab19a2e35373985
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-25 22:59:14 -0500

    add task5

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+A Rectangle
+"""
+
+
+class Rectangle:
+    """
+    functions and data
+    """
+    def __init__(self, width=0, height=0):
+        """instatiation
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter width
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter height
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns perimeter
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """hsajwushw"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            string = ""
+            for a in range(self.__height):
+                if a == self.__height - 1:
+                    string += self.__width * '#'
+                else:
+                    string += self.__width * '#' + '\n'
+            return string
+
+    def __repr__(self):
+        """Returns a representation of printable string"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """ Destructors are called when an object gets destroyed """
+        print("Bye rectangle...")
