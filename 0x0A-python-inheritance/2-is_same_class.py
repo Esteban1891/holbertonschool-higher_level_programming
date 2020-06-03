0632081a44f58bf34829ac472e74200eb805d8c2
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-02 19:23:03 -0500

    add task2 if return a boolean

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""initializate"""
+
+
+def is_same_class(obj, a_class):
+    """
+    :param obj:
+    :param a_class:
+    :return:
+    """
+    return(type(obj) == a_class)
+
