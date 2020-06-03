282af92fa40f3f980d34a9c67e013a4e6df70e07
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-02 21:26:35 -0500

    task7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,6 +12,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """validator"""
         if type(value) is not int:
-            raise TypeError(name + "must be an integer")
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError(name + "must be greater than 0")
+            raise ValueError(name + " must be greater than 0")
