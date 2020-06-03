9826c21749169fd20447de88388d8741dc412836
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-02 22:46:28 -0500

    task 8 and 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,4 +19,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """str"""
-        return "[rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
