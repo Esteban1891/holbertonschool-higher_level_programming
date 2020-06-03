7cae6980c56be50a70c91a51fb18bceb8e877f96
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-02 23:57:51 -0500

    task 13 advance

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""initializate"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Adds a new attribute to an object if it’s possible
+    """
+    if hasattr(obj, '__dict__'):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
