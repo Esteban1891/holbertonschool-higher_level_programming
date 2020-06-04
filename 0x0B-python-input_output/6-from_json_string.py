93408659103b9f16c749a0b291012ddd1ace403b
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-03 21:16:57 -0500

    task6

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -4,5 +4,6 @@ import json
 
 
 def from_json_string(my_str):
-    """returns an object (Python data structure) represented by a JSON string"""
+    """returns an object (Python data structure)
+     represented by a JSON string"""
     return json.loads(my_str)
