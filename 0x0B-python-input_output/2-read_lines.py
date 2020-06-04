60052e4ae41fee682ef6b3c36455743494bed79d
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-03 20:41:21 -0500

    task2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""initializate"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """function that reads n lines of a text
+     file (UTF8) and prints it to stdout"""
+    a = len(open(filename).readline())
+    with open(filename, encoding="utf-8") as f:
+        if nb_lines <= 0 or filename >= a:
+            lines = f.read()
+            print(lines, end="")
+        else:
+            for _ in range(nb_lines):
+                lines = next(f).strip()
+                print(lines)
+    f.close()
