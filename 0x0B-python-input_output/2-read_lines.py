d55e89a017bff50570bae04ab80c92a3fa329509
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-03 20:46:47 -0500

    task2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,13 +3,12 @@
 
 
 def read_lines(filename="", nb_lines=0):
-    """function that reads n lines of a text
-     file (UTF8) and prints it to stdout"""
-    a = len(open(filename).readline())
+    """ function that reads n lines of a text file (UTF8) and prints it to stdout"""
+    a = len(open(filename).readlines())
     with open(filename, encoding="utf-8") as f:
         if nb_lines <= 0 or nb_lines >= a:
-            lines = f.read()
-            print(lines, end="")
+            line = f.read()
+            print(line, end="")
         else:
             for _ in range(nb_lines):
                 lines = next(f).strip()
