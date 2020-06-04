e728a48be16da6a1e77affcf65925bbdacbaba2c
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-03 20:55:16 -0500

    task2

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""initializate"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file (UTF8)
+     and returns the number of characters written"""
+    with open(filename, 'w', text="") as f:
+        f.write(text)
+    f.close()
+    return len(text)
