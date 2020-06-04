28c3fa0a6a0acd9303f098a689f052e72f4f20e9
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-03 21:25:41 -0500

    task6

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""initializate"""
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file"""
+    with open(filename, 'w') as f:
+        file_json = json.dumps(my_obj)
+        f.write(file_json)
+    f.close()
