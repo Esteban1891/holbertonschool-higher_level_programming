a399d7548a6bc192fafb45ca2bd17e41e2bafcbd
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-03 20:26:32 -0500

    task1

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""initializate"""
+
+
+def number_of_lines(filename=""):
+    """ that returns the number of lines of a text file"""
+    n = 0
+    with open(filename, encoding="utf-8") as f:
+        line = f.readline()
+        n = len(line)
+    f.close()
+    return(n)
