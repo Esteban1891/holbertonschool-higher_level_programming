cc6c9f5379d359a00063a7ff13f6963bbb41b4e4
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-03 23:20:17 -0500

    task12

---
+++ 0x05-python-exceptions/13-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""initializate"""
+
+
+class Student:
+    """create class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializate atrributes
+        :param first_name:
+        :param last_name:
+        :param age:
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json
+        """
+        dic = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if hasattr(self, i):
+                dic[i] = getattr(self, i)
+        return dic
+
+   def reload_from_json(self, json):
+        """ defines a student """
+        for key, value in json.items():
+            self.__dict__[key] = value
+
