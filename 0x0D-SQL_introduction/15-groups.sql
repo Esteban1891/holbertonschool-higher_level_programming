12b9c9be706ab548a325fede53e4f185530479a7
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 18:43:26 -0500

    add task-15 listing # of records with same score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+--  script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT score, count(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
