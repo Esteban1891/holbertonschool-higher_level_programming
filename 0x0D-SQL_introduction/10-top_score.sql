b9a3b1c8540de7c98b0e87172bd5a665018c6b8c
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 16:57:35 -0500

    add task-10  script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- listed in descending order
-INSERT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
+
