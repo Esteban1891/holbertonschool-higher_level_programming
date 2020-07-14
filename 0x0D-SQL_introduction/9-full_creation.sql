db44d9368d12d5db49cef5d9a085816c192af501
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 16:25:46 -0500

    add task-9 creating second_table and creating multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ id INT,
 name VARCHAR(256),
 score INT
 );
-INSERT INTO `second_table` (´id´ = 1, ´name´ = “John”, ´score´ = 10);
-INSERT INTO `second_table` (´id´ = 2, ´name´ = “Alex”, ´score´ = 3);
-INSERT INTO `second_table` (´id´ = 3, ´name´ = “Bob”, ´score´ = 14);
-INSERT INTO `second_table` (´id´ = 4, ´name´ = “George”, ´score´ = 8);
+INSERT INTO `second_table` (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (id, name, score) VALUES (4, 'George', 8);
