aaff459d28e31ff4430e6adab359f8aa9df99182
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 17:13:56 -0500

    add task-11 that lists all records with a score >= 10 in the table second_table

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server
--- listing all scores >= 10 in descending order
+-- listing all scores >= 10 in descending order.
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
