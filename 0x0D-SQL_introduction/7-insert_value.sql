e31ee3b58cfdef4db0bce677fae36caba0b8bd65
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 15:43:22 -0500

    add task-7 and solution in error the code fail what is not put id and name

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,4 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-INSERT INTO `first_table` (
-id 89,
-name Holberton School
+INSERT INTO `first_table` VALUES (
+	89, 'Holberton School'
 );
