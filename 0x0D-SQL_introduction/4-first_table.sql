30544a0d548a54e4630a8fd6532aca480a21039f
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 14:07:28 -0500

    add task-4 that create table call first_table after add id else name

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- create table called first_table in the current database in your MySQL server.
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, your script should not fail
-CREATE DATABASE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	NAME VARCHAR(256)
 );
