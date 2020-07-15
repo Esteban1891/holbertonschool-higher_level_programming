42d5b88dd3733b3ff0171555ffc9dbbb2dc72cc5
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 22:58:43 -0500

    add task-0 for grants of user in localhost

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---listing all privileges of user_0d_1 and user_0d_2
+-- listing all privileges of user_0d_1 and user_0d_2 
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
