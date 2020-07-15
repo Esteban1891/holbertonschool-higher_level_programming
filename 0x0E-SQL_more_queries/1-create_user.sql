dc8a938a0da73faf4c087a06f0d89faa0fde87a5
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 23:09:52 -0500

    add task-1 create server user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script that creates the MySQL server user user_0d_1
 
-CREATE USER 'user_0d_1' IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER 'user_0d_1' IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
