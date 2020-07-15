95c8b748b6076039a026103d17751a52aff0ea14
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-14 19:03:15 -0500

    add task-advance

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- displays average temperature by city ordered by temperature
+SELECT city, AVG(value) AS 'avg_temp' FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,2 @@
+-- displays max temperature of each state ordered by state name
+SELECT state, MAX(value) AS 'max_temp' FROM temperatures GROUP BY state ORDER BY state;
