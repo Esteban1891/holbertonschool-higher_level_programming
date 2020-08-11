f1d49917d008550b98c67ba8573c46da473c3faf
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-10 21:58:33 -0500

    add task3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,17 +7,21 @@ but avoiding SQL injection.
 from sys import argv
 import MySQLdb
 
-if __name__ == '__main__':
 
-    # declaring arguments passed, with query.
+def safe_filter():
+    """declaring arguments passed, with query"""
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
     name = argv[4]
 
     # creating connection to the database.
-    db_connection = MySQLdb.connect(host="localhost", port=3306, user=user,
-                                    passwd=passwd, db=db, charset="utf8")
+    db_connection = MySQLdb.connect(host="localhost",
+                                    port=3306,
+                                    user=user,
+                                    passwd=passwd,
+                                    db=db,
+                                    charset="utf8")
 
     # Making a cursor Object for query execution.
     cursor = db_connection.cursor()
@@ -34,3 +38,7 @@ if __name__ == '__main__':
 
     cursor.close()
     db_connection.close()
+
+
+if __name__ == '__main__':
+    safe_filter()
