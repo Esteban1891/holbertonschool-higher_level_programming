abf5100513423ef70117813abc58a8cdb3278111
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-12 13:20:31 -0500

    add task7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""cript that lists all State objects
+   from the database hbtn_0e_6_usa"""
+from sqlalchemy import (create_engine)
+from model_state import State, Base
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+def model_state():
+    """initializate function model_state for db"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # associate it with our custom Session class
+    Session = sessionmaker()
+
+    # associate it with our custom Session class
+    Session.configure(bind=engine)
+
+    session = Session()
+
+    rows = session.query(State).order_by(State.id).all()
+
+    for state in rows:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close
+
+
+if __name__ == '__main__':
+    model_state()
