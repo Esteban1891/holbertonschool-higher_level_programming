1e18d978b3f19eedaf7608b81a8d5142638df234
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-24 21:32:33 -0500

    add task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""initializate"""
+import requests
+
+
+def status():
+    """a Python script that fetches https://intranet.hbtn.io/status"""
+
+    data_req = request.get('https://intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}".
+          format(type(data_req.text), data_req.text))
+
+
+if __name__ == '__main__':
+    status()
