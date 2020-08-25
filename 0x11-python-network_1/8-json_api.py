da4567f094abb1bffcc6f0ad2a6ddb35810496a3
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-24 22:58:12 -0500

    add task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request to a URL
+Uses the letter as a parameter
+"""
+import requests
+from sys import argv
+
+
+def json_api():
+        try:
+            q = argv[1]
+        except:
+            q = ''
+            url = 'http://0.0.0.0:5000/search_user'
+            payload = {'q': q}
+            r = requests.post(url, payload)
+        try:
+            json = r.json()
+            if len(json) == 0:
+                print('No result')
+            else:
+                json_id = json.get('id')
+                json_name = json.get('name')
+                print('[{}] {}'.format(json_id, json_name))
+        except:
+            print('Not a valid JSON')
+
+
+if __name__ == '__main__':
+    json_api()
