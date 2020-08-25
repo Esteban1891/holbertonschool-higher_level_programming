92b6c892603e289fcb059ac2266334113b8dc3e4
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-24 21:51:56 -0500

    add task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""initializate"""
+from sys import argv
+import requests
+
+
+def header():
+    """
+    Script that fetches to url and get X-Request-Id.
+    """
+    req = requests.get(argv[1])
+    print(type(req.headers.get('X-Request-Id')))
+
+
+if __name__ == '__main__':
+    header()
