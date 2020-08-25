bb8c551df54739258984cc9dfaebe05d82b49f3b
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-24 21:35:37 -0500

    add task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 def status():
     """a Python script that fetches https://intranet.hbtn.io/status"""
 
-    data_req = request.get('https://intranet.hbtn.io/status')
+    data_req = requests.get('https://intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}".
           format(type(data_req.text), data_req.text))
 
