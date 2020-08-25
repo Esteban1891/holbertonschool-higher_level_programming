16daf2d6d82c4fae2f911a4fc06fb6e099e0d9a1
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-24 23:39:41 -0500

    add task 103

---
+++ 0x11-python-network_1/103-search_twitter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+takes in 3 strings and sends a search request to the Twitter API
+"""
+import requests
+from base64 import b64encode
+from sys import argv
+
+
+def searh_twitter():
+    key_secret = argv[1] + ':' + argv[2]
+    base64_encode = b64encode(key_secret.encode()).decode('utf-8')
+    payload = {'grant_type': 'client_credentials'}
+    header = {'Authorization': 'Basic {}'.format(base64_encode),
+              'Content-Type':
+              'application/x-www-form-urlencoded;charset=UTF-8'}
+    r = requests.post('https://api.twitter.com/oauth2/token', headers=header,
+                      data=payload)
+    token = r.json()
+    b = token.get('access_token')
+    header = {'Authorization': 'Bearer {}'.format(b)}
+    payload = {'q': argv[3], 'count': 5}
+    r = requests.get('https://api.twitter.com/1.1/search/tweets.json',
+                     headers=header, params=payload)
+    response = r.json()
+    tweets = response.get('statuses')
+    for tweet in tweets:
+        print('[{}] {} by {}'.format(tweet.get('id'), tweet.get('text'),
+                                     tweet.get('user').get('name')))
+
+
+if __name__ == '__main__':
+    searh_twitter()
