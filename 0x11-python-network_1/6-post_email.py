36482846c31f6761fc66de80b7248f20c15df110
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-24 22:16:08 -0500

    add task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""initializate"""
+from sys import argv
+from urllib import requests
+
+
+def post_email():
+    """ URL and an email address, sends a POST request to the passed URL with
+        the email as a parameter, and finally displays the body of the response.
+    """
+    data_mail = {'email': argv[2]}
+    response = requests.post(argv[1], data=data_mail)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    post_email()
