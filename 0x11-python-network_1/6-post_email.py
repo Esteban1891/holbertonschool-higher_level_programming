d6bcfd6f5706aa868b8c2f2755b2847226dadb06
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-24 22:20:07 -0500

    add task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,8 +5,9 @@ import requests
 
 
 def post_email():
-    """ URL and an email address, sends a POST request to the passed URL with
-        the email as a parameter, and finally displays the body of the response.
+    """ URL and an email address, sends a POST request
+        to the passed URL with the email as a
+        parameter, and finally displays the body of the response.
     """
     data_mail = {'email': argv[2]}
     response = requests.post(argv[1], data=data_mail)
