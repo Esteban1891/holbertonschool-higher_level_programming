f55cb4a1d5671c9eee9f36a557b8100906b9b97f
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-24 22:49:20 -0500

    add task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""initilizate"""
+from sys import argv
+import requests
+
+
+def error_code():
+    """"""Script that fetches to url and get X-Request-Id."""
+    res = requests.get(argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
+
+
+if __name__ == '__main__':
+    error_code()
