82996619555fe25ae693498d4e467ee2060a2c0d
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-15 20:55:00 -0500

    add task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const firstLine = 'C is fun', secondLine = 'Python is cool';
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
 const thirdLine = 'Javascript is amazing';
 console.log(firstLine);
 console.log(secondLine);
