8f77bf1ba7cfaf0ecd8c62c5575cac0b0fc478dc
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-15 23:33:28 -0500

    add task7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-let convert = process.argv[2];
-convert = parseInt(x);
-if (isNaN(convert)) {
+let x = process.argv[2];
+x = parseInt(x);
+if (isNaN(x)) {
   console.log('Missing number of occurences');
 } else {
-  for (let i = 0; i < convert; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
+
