5da5e0d39adf292909f4efd43a92b8c6f5445d25
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-15 21:22:29 -0500

    add task2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length <= 2) {
+  console.log('no argument');
+} else if (process.argc.length > 3) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
