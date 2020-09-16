2ad19fac4d24dd2cd1064073f216551c1bb9f621
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-15 23:40:51 -0500

    add task8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let squarePrinter = 'X';
+let size = process.argv[2];
+size = parseInt(size);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else if (size > 0) {
+  for (let i = 0; i < size; i++) {
+    console.log(squarePrinter.repeat(size));
+  }
+}
