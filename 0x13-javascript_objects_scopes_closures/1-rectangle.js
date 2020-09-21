ed75b645d42c0e212a8464aaf8f059c1ae966c36
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-21 08:14:32 -0500

    add task 1 initializate class rectangle with 'this'

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// class Rectangle that defines a Rectangle
+/*
+ * You must use the class notation for defining your class
+ * The constructor must take 2 arguments w and h
+ * Initialize the instance attribute width with the value of w
+ * Initialize the instance attribute height with the value of h
+ */
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
