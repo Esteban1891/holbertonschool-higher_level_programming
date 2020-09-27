73943739e7adcabf0b25fc24527b315780e1512a
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-27 05:49:58 -0500

    add task8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,11 @@
 $(document).ready(function updateAll () {
   $.get('https://swapi-api.hbtn.io/api/films/?format=json', function updateMovies (data) {
     const list_movies = data.results; const length_list = list_movies.length;
-    for (let i = 0; i <= length_list; i++) {
+    let i = 0;
+    while (i <= length_list) {
       const movie_title = list_movies[i].title; const content = $('<li></li>').text(movie_title);
       $('ul#list_movies').append(content);
+      i++;
     }
   });
 });
